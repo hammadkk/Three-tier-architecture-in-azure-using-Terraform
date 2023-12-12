035f044f590cee7356cb3a9f7e2686ae901c7a7c
hammadkk <hammadsaif061@gmail.com>
2023-12-12 21:10:06 +0500

    Updated main file

--- main.tf
+++ main.tf
@@ -14,3 +14,24 @@ module "network" {
   resource_group_name = var.resource_group_name
   vnet_name = var.vnet_name
 }
+
+module "subnets" {
+  source = "./subnets"
+  resource_group_location = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  vnet_name = var.vnet_name
+  subnet_names = var.subnet_names
+  vnet_id = module.network.vnet_id
+}
+
+module "nsgs" {
+  source = "./nsgs"
+  resource_group_location = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  vnet_name = var.vnet_name
+  subnet_names = var.subnet_names
+  default_nsg_rules = var.default_nsg_rules
+  custom_nsg_rules = var.custom_nsg_rules
+  subnet_ids = module.subnets.subnet_ids
+  vnet_id = module.network.vnet_id
+}
